f159f86934905cd0bf432a2176de43489edffb37
AbelAlejandroDev <pyschofaint@gmail.com>
2024-06-27 03:50:34 -0400

    Set blurs in logo

--- script.js
+++ script.js
@@ -1,47 +1,39 @@
-document.addEventListener('DOMContentLoaded', function() {
+const w = window
+const d = document
+
+document.addEventListener('DOMContentLoaded', function () {
     const logoContainer = document.getElementById('logo-container');
-    const logo = document.querySelector('#logo img');
+    const logoImg = document.querySelector('#logo img');
     const overlay = document.getElementById('overlay');
-    const loading = document.getElementById('loading');
+    const loading = d.getElementById('loading');
 
-    // Mostrar loading inicialmente
-    loading.style.display = 'flex';
-
-    // Función para ocultar el loading con efecto de desvanecimiento
-    function ocultarLoading() {
-        loading.style.opacity = '0';
-        setTimeout(() => {
-            loading.style.display = 'none';
-        }, 500);
-    }
-
-    // Función para abrir las puertas al hacer clic en el logo
     function abrirPuertas() {
-        logo.classList.add('rotate');
+        logoImg.classList.add('rotate', 'blue-blur');
 
         // Esperar a que la rotación termine antes de abrir las puertas
         setTimeout(() => {
             overlay.style.display = 'block';
             setTimeout(() => {
                 logoContainer.classList.add('open');
-            }, 50);
+            }, 150);
 
             // Ocultar puertas y logo después de la animación
             setTimeout(() => {
                 overlay.style.display = 'none';
-            }, 2000);
+            }, 1500);
         }, 1000);
     }
 
-    // Evento que se dispara cuando todas las imágenes están cargadas
-    window.addEventListener('load', function() {
+
+    window.addEventListener('load', function () {
         setTimeout(() => {
-            ocultarLoading();
-        }, 2000);
+            loading.classList.add('hidden')
+        }, 1500);
     });
 
     // Evento clic en el logo para abrir las puertas
-    logoContainer.addEventListener('click', function() {
+    logoContainer.addEventListener('click', function () {
         abrirPuertas();
     });
-});
+
+})
